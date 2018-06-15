b6b6855f620bbb638eb4c79f722f3dd80490022d
PastLeo <chgu82837@gmail.com>
2018-06-15 18:11:13 +0800

    demo shadow dom

--- src/hello-custom-ele.js
+++ src/hello-custom-ele.js
@@ -1,7 +1,11 @@
 class HelloCustomEle extends HTMLElement {
   constructor() {
-    super();
-    debugger
+    super()
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.innerHTML = `
+      <style> h1 { color: #5bbd72; } </style>
+      <h1>hello-custom-ele rendered!</h1>
+    `
   }
 }
 customElements.define('hello-custom-ele', HelloCustomEle);
