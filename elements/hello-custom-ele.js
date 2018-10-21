99aa869af42ad80a680e65cc5e2817cf0feb3174
PastLeo <chgu82837@gmail.com>
2018-10-21 16:29:20 +0800

    hello lit-html

--- elements/hello-custom-ele.js
+++ elements/hello-custom-ele.js
@@ -1,14 +1,19 @@
+import { html, render } from 'https://unpkg.com/lit-html@0.12.0/lit-html.js?module'
+
 class HelloCustomEle extends HTMLElement {
   connectedCallback() {
     this.attachShadow({mode: 'open'});
-    this.shadowRoot.innerHTML = `
-      <style> h1 { color: green; } </style>
-      <link href='./elements/hello-custom-ele.css' rel='stylesheet'>
-      <h1>
-        <span>hello-custom-ele</span>
-        rendered!
-      </h1>
-    `
+    render(
+      html`
+        <style> h1 { color: green; } </style>
+        <link href='./elements/hello-custom-ele.css' rel='stylesheet'>
+        <h1>
+          <span>hello-custom-ele</span>
+          rendered!
+        </h1>
+      `,
+      this.shadowRoot
+    )
   }
 }
 
